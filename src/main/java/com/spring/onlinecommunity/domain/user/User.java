67eb05ac8860ad35556a838a9e8fdc76e793ad65
PersesTitan <torampersesTitan@gmail.com>
2022-12-07 07:43:02 +0900

    update add following, follower, boards, service logic

--- src/main/java/com/spring/onlinecommunity/domain/user/User.java
+++ src/main/java/com/spring/onlinecommunity/domain/user/User.java
@@ -1,35 +1,70 @@
 package com.spring.onlinecommunity.domain.user;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import com.spring.onlinecommunity.domain.board.Board;
+import com.spring.onlinecommunity.domain.both.TimeSetting;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import lombok.ToString;
 
-import java.time.LocalDateTime;
+import java.util.*;
 
-@Entity @Getter
+import static jakarta.persistence.FetchType.*;
+
+@Getter
+@Entity
+@Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
-    @Id @GeneratedValue
-    @Column(name = "user_id")
+public class User extends TimeSetting {
+    @Id
+    @GeneratedValue
     private Long id;
 
-    @Column(name = "user_username", unique = true, nullable = false)
+    @Column(unique = true, nullable = false)
     private String username;
-    @Column(name = "user_password", nullable = false)
+    @Column(nullable = false)
     private String password;
 
-    @CreatedDate
-    private LocalDateTime createDate;
+    @OneToMany(targetEntity = User.class, fetch = LAZY, mappedBy = "id")
+    private final Set<User> following = new LinkedHashSet<>();
 
-    @LastModifiedDate
-    private LocalDateTime updateDate;
+    @OneToMany(targetEntity = User.class, fetch = LAZY, mappedBy = "id")
+    private final Set<User> follower = new LinkedHashSet<>();
 
+    @OneToMany(targetEntity = Board.class, fetch = LAZY, mappedBy = "id")
+    private final Set<Board> boards = new LinkedHashSet<>();
+
+    private User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public static User createUser(UserVO userVO) {
+        String username = userVO.username();
+        String password = userVO.password();
+        return new User(username, password);
+    }
+
+    /**
+     * 서비스 로직
+     * @param user 팔로잉하는 유저
+     */
+    public void addFollow(User user) {
+        this.following.add(user);
+        user.follower.add(user);
+    }
+
+    public void removeFollow(User user) {
+        this.following.remove(user);
+        user.follower.remove(user);
+    }
+
+    public int getFollowingSize() {
+        return this.following.size();
+    }
+
+    public int getFollowerSize() {
+        return this.follower.size();
+    }
 }
